ccfadc5535be95df29680de74e91071c9fcd06ca
Francesco Anselmo <francesco.anselmo@arup.com>
2017-11-05 20:41:37 +0000

    Added service cron restart for install script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 sudo apt-get install python-pip python-dev python-scipy python-matplotlib python-wheel
 sudo pip install -r requirements.txt
 cp ocean.cron /etc/cron.d/ocean
-
+service cron restart
 
